6b27cb9bc52da709fcf38336658522b276ba145f
Christopher Hanley <chanley@gmail.com>
2014-04-03 20:38:13 -0400

    Simplify import and try/except statements.

--- imexam/__init__.py
+++ imexam/__init__.py
@@ -2,21 +2,13 @@
 
 from .util import *
 import connect as _connect
-connect=_connect.Connect
+connect = _connect.Connect
 
 try:
     import astropy
 except ImportError:
     print("astropy not loaded, using regular pyfits")
-    has_astropy = False
-else:
-    has_astropy = True
-
-if has_astropy:
-    from astropy.io import fits as pyfits
-else:
     try:
         import pyfits
     except ImportError:
         print("pyfits is not installed, please install for full functionality")
-        
