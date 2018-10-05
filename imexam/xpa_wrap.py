276b676703ec92ae293622f6f4823e6566fc4618
heatherkurtz <hkurtz@stsci.edu>
2018-10-05 08:40:59 -0400

    python3

--- imexam/xpa_wrap.py
+++ imexam/xpa_wrap.py
@@ -1,6 +1,6 @@
 """Licensed under a 3-clause BSD style license - see LICENSE.rst."""
 
-from __future__ import print_function, division, absolute_import
+#from __future__ import division, absolute_import
 from xpa import xpa
 
 
