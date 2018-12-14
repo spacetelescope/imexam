a5b732bd4f0dbd91f8ccb7d4204840d237e526c5
Megan Sosey <sosey@stsci.edu>
2018-12-14 15:59:51 -0500

    going back to development

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ builtins._ASTROPY_PACKAGE_NAME_ = PACKAGENAME
 
 
 # VERSION should be PEP386 compatible (http://www.python.org/dev/peps/pep-0386)
-VERSION = '0.8.1'
+VERSION = '0.8.2dev'
 
 # Indicates if this version is a release version
 RELEASE = 'dev' not in VERSION
