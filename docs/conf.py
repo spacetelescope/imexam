82b3b82f39f32785015d089a8cfd2445ba90c2bf
Zach Burnett <zburnett@stsci.edu>
2022-09-20 13:18:51 -0400

    use canonical filename of RTD configuration and enable `nitpicky` mode

--- docs/conf.py
+++ docs/conf.py
@@ -149,7 +149,7 @@ github_issues_url = f'https://github.com/{github_project}/issues/'
 
 
 # -- Turn on nitpicky mode for sphinx (to warn about references not found) ----
-nitpicky = False
+nitpicky = True
 nitpick_ignore = []
 
 # Some warnings are impossible to suppress, and you can list specific
